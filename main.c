1ae77d357192d6f2bb8f2b7f395fa0fc898bb7c1
Andre-Basile <andrekovohouande@gmail.com>
2025-08-15 20:13:17 +0200

    first update

---
+++ main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,tab[40][50];
+    i=0;
+    getch();
+
+     printf("\n\t\t\t\t");
+     printf("P ");
+    while(i<40)
+    {
+        j=1;
+
+        while(j<50)
+        {
+            printf(". ");
+            j++;
+        }
+        printf("\n\t\t\t\t");
+        i++;
+    }
+    int P,X,Y;
+    P=0;
+
+    if(input=='P')
+    {
+        printf
+    }
+
+
+    return 0;
+}
